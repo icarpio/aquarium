61dde50562c224e418c9fe614a68b4bb91debb93
Ivan <icarpiodeveloper@gmail.com>
2026-03-01 10:52:29 +0100

    To prod

--- js/shared.js
+++ js/shared.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:8000';
+const API_BASE = 'https://marianoapi.onrender.com';
 const SOURCE   = 'aquarium';
 
 function getToken() { return localStorage.getItem('aq_token'); }
